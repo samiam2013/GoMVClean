7a78507fbe57c8685e4879386080caa9eb184d58
Sam <samiam2013@gmail.com>
2020-02-01 12:08:07 -0600

    update a comment and remove superfluous html

--- controller.go
+++ controller.go
@@ -14,7 +14,7 @@ const indexName string = "index"
 const indexPathName string = indexPath + indexName
 const indexFAIL = "404"
 
-// if path == / , load homePage.html. otherwise 404
+// if path ("/"|"/indexName") serve indexName+staticMarkupType otherwise 404
 func routeIndex(w http.ResponseWriter, r *http.Request) {
 	path := r.URL.Path
 	if path == indexPath || path == indexPathName {
