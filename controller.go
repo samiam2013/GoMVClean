20bc5e5588482efbf35adb8844408edd57cd666b
Sam <samiam2013@gmail.com>
2020-02-12 01:46:22 -0600

    separate index router

--- controller.go
+++ controller.go
@@ -11,30 +11,13 @@ import (
 const gDebug bool = true
 
 const httpsPort string = ":443"
-const indexPath string = "/"
-const indexName string = "index"
-const indexPathName string = indexPath + indexName
-const indexFAIL = "404"
-
 const tlsPath = "TLS"
 const tlsFolder = tlsPath + string(os.PathSeparator)
 const tlsKey = tlsFolder + "snakeoil.key"
 const tlsCert = tlsFolder + "snakeoil.cert"
 
-// if path ("/"|"/indexName") serve indexName+staticMarkupType otherwise 404
-func routeIndex(w http.ResponseWriter, r *http.Request) {
-	path := r.URL.Path
-	if path == indexPath || path == indexPathName {
-		path = staticMarkupFolder + indexName + staticMarkupType
-		renderStatic(path, w, r)
-		return
-	}
-	errorShortCircuit(w, r, indexFAIL)
-	return
-}
-
 func main() {
-	http.HandleFunc(indexPath, routeIndex)
+	http.HandleFunc(indexPath, routeIndex)   // found in index.go
 	http.HandleFunc(errorsPath, routeError)  // found in errors.go
 	http.HandleFunc(staticPath, routeStatic) // found in static.go
 	http.HandleFunc(viewPath, routeView)     // found in view.go
---
+++ index.go
@@ -0,0 +1,20 @@
+package main
+
+import "net/http"
+
+const indexPath string = "/"
+const indexName string = "index"
+const indexPathName string = indexPath + indexName
+const indexFAIL = "404"
+
+// if path ("/"|"/indexName") serve indexName+staticMarkupType otherwise 404
+func routeIndex(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+	if path == indexPath || path == indexPathName {
+		path = staticMarkupFolder + indexName + staticMarkupType
+		renderStatic(path, w, r)
+		return
+	}
+	errorShortCircuit(w, r, indexFAIL)
+	return
+}
